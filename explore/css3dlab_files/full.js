4e8756a36d31cbc9a22862a18fa9a74bf87c59dc
Suhail Sherif <suhail.sherif@gmail.com>
2021-02-25 13:47:28 +0530

    All updated

--- explore/css3dlab_files/full.js
+++ explore/css3dlab_files/full.js
@@ -116,47 +116,127 @@ var regioncolours = {}
 regioncolours[1] = "url('css3dlab_files/brickwall.jpg')";
 regioncolours[2] = regioncolours[21] = "url('css3dlab_files/astralwall.jpg')";
 regioncolours[3] = regioncolours[31] = regioncolours[39] = "yellow";
-regioncolours[4] = regioncolours[49] = "green";
-regioncolours[5] = regioncolours[59] = "orange";
-regioncolours[6] = regioncolours[69] = "blue";
+regioncolours[4] = regioncolours[41] = regioncolours[49] = "green";
+regioncolours[5] = regioncolours[51] = regioncolours[59] = "orange";
+regioncolours[6] = regioncolours[61] = regioncolours[69] = "blue";
 regioncolours[9] = regioncolours[99] = "white";
 var currentregion=10;
 var map = [
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,2,2,2,2,2,2,0,59,59,5,5,0,6,6,69,69,0,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,0,0,0,0,5,0,6,0,0,0,0,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,0,5,0,6,0,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,0,5,0,6,0,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,0,0,0,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,0,1,1,2,2,2,2,2,2,2,2,2,2,2,0],
-	[0,0,2,2,2,2,2,2,2,2,2,2,0,1,0,2,2,2,2,2,2,2,2,2,2,0,0],
-	[0,0,3,0,2,2,2,2,2,2,2,2,0,1,0,2,2,2,2,2,2,2,2,0,4,0,0],
-	[0,0,3,0,0,2,0,0,2,2,2,2,0,1,0,2,2,2,2,0,0,2,0,0,4,0,0],
-	[0,0,3,0,0,2,2,0,0,2,2,2,0,1,0,2,2,2,0,0,2,2,0,0,4,0,0],
-	[0,0,3,31,0,0,2,2,0,0,2,2,0,0,0,2,2,0,0,2,2,0,0,4,4,0,0],
-	[0,0,0,31,0,0,0,2,2,0,0,0,0,0,0,0,0,0,21,2,0,0,0,4,0,0,0],
-	[0,0,39,31,0,0,0,0,9,9,9,9,9,9,9,9,9,9,99,0,0,0,0,4,49,49,0],
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 0,59,59,51, 5, 0,61,61,69,69, 0, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 5, 0, 6, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 5, 0, 6, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 5, 2, 6, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,0],
+	[0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,0],
+	[0, 0, 3, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 4, 0,0],
+	[0, 0, 3, 0, 0, 2, 0, 0, 2, 2, 2, 2, 0, 1, 0, 2, 2, 2, 2, 0, 0, 2, 0, 0, 4, 0,0],
+	[0, 0, 3, 0, 0, 2, 2, 0, 0, 2, 2, 2, 0, 1, 0, 2, 2, 2, 0, 0, 2, 2, 0, 0, 4, 0,0],
+	[0, 0, 3,31, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0, 0, 2, 2, 0, 0, 2, 2, 0, 0,41,41, 0,0],
+	[0, 0, 0,31, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,21, 2, 0, 0, 0,41, 0, 0,0],
+	[0, 0,39,31, 0, 0, 0, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,99, 0, 0, 0, 0,41,49,49,0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0]
 ];
 var unlocked = [1,2,9,99];
 var walls = {};
 var currvid = '';
 var ponyexit = false;
 var gamestarted = false;
+var gameended = false;
 var bgmmuted = false;
 
+function ease(t) { return Math.cos(Math.PI*(1-t)/2)**4; }
+function updateMusicByDistances(x,y){
+	var bgm = document.getElementById('bgm');
+	var bdm = document.getElementById('bdm');
+	var xindex = Math.floor(x/wallw);
+	var yindex = Math.floor(y/wallw);
+	if(xindex<0 || xindex >= map.length || yindex < 0 || yindex >= map[0].length) return;
+	var reg = map[xindex][yindex];
+	var dist;
+	if(reg==2 || reg==21){
+		// Adjust bgm, silence downwards
+		if(xindex==19 && (yindex==8 || yindex==18)){
+			dist = 500-x%wallw;
+			if(dist<250){
+				bgm.volume = .3*ease(dist/250);
+			}
+		}
+		// Adjust bgm, silence bottomrightwards
+		if(xindex==19 && yindex==7){
+			dist = Math.sqrt((x-(xindex+1)*wallw)**2 + (y-(yindex+1)*wallw)**2);
+			if(dist<250){
+				bgm.volume = .3*ease(dist/250);
+			}
+		}
+		// Adjust bgm, silence bottomleftwards
+		if(xindex==19 && yindex==19){
+			dist = Math.sqrt((x-(xindex+1)*wallw)**2 + (y-yindex*wallw)**2);
+			if(dist<250){
+				bgm.volume = .3*ease(dist/250);
+			}
+		}
+	} else if([9,99].includes(reg)){
+		// Adjust bdm, silence upwards
+		if(xindex==20 && (yindex==8 || yindex==18)){
+			dist = x%wallw;
+			if(dist<250){
+				bdm.volume = 1*ease(dist/250);
+			}
+		}
+		// Adjust bdm, silence topleftwards
+		if(xindex==20 && yindex==9){
+			dist = Math.sqrt((x-xindex*wallw)**2 + (y-yindex*wallw)**2);
+			if(dist<250){
+				bdm.volume = 1*ease(dist/250);
+			}
+		}
+		// Adjust bdm, silence toprightwards
+		if(xindex==20 && yindex==17){
+			dist = Math.sqrt((x-xindex*wallw)**2 + (y-(yindex+1)*wallw)**2);
+			if(dist<250){
+				bdm.volume = 1*ease(dist/250);
+			}
+		}
+	} else if(reg%10!=9 && map[xindex][yindex+1]%10==9){
+		// Adjust bgm, silence rightwards
+		dist = 500-y%wallw;
+		if(dist<250){
+			bgm.volume = .3*ease(dist/250);
+		}
+	} else if(reg%10!=9 && map[xindex][yindex-1]%10==9){
+		// Adjust bgm, silence leftwards
+		dist = y%wallw;
+		if(dist<250){
+			bgm.volume = .3*ease(dist/250);
+		}
+	} else if(reg%10!=9 && map[xindex+1][yindex+1]%10==9){
+		// Adjust bgm, silence bottomrightwards
+		dist = Math.sqrt((x-(xindex+1)*wallw)**2 + (y-(yindex+1)*wallw)**2);
+		if(dist<250){
+			bgm.volume = .3*ease(dist/250);
+		}
+	} else if(reg%10!=9 && map[xindex+1][yindex-1]%10==9){
+		// Adjust bgm, silence bottomleftwards
+		dist = Math.sqrt((x-(xindex+1)*wallw)**2 + (y-yindex*wallw)**2);
+		if(dist<250){
+			bgm.volume = .3*ease(dist/250);
+		}
+	}
+}
 function findRegion(x,y){
 	var xindex = Math.floor(x/wallw);
 	var yindex = Math.floor(y/wallw);
 	if(xindex < 0 || xindex >= map.length) return -1;
-	if(xindex < 0 || yindex >= map[0].length) return -1;
+	if(yindex < 0 || yindex >= map[0].length) return -1;
 	return map[xindex][yindex];
 }
 
@@ -174,37 +254,81 @@ var covered = map.map(function (row) { return row.map(function (entry) { return 
 
 function checkbk1answer(){
 	var ans = document.getElementById('bk1ans').value.toLowerCase();
-	if(ans == "hbd"){
+	if(ans.includes("maggi")){
 		walls["14,2,2"].node.remove();
 		unlocked.push(3);
 		document.getElementById('key').play();
 	}
 }
 function checkbk2answer(){
-	var ans = document.getElementById('bk2ans').value.toLowerCase();
-	if(ans == "hbd"){
-		walls["18,2,1"].node.remove();
-		unlocked.push(31);
-		document.getElementById('key').play();
-	}
+	walls["18,2,1"].node.remove();
+	unlocked.push(31);
+	document.getElementById('key').play();
 }
 function checkbk3answer(){
 	walls["20,3,3"].node.remove();
 	unlocked.push(39);
 	document.getElementById('key').play();
 }
-
-
-function checksuhail2answer(){
-	console.log("Checking");
-	var ans = document.getElementById('suhail2ans').value.toLowerCase();
+function checksuhail1answer(){
+	var ans = document.getElementById('suhail1ans').value.toLowerCase();
 	if(ans == "hbd"){
 		walls["14,24,2"].node.remove();
 		unlocked.push(4);
+		document.getElementById('key').play();
+	}
+}
+function checksuhail2answer(){
+	var ans = document.getElementById('suhail2ans').value.toLowerCase();
+	if(ans == "beyonce"){
+		walls["17,24,2"].node.remove();
+		unlocked.push(41);
 		unlocked.push(49);
 		document.getElementById('key').play();
 	}
 }
+function checkshirley1answer(){
+	var ans = document.getElementById('shirley1ans').value.toLowerCase();
+	if(ans.includes("shirley") || ans.includes("anniversary") || ans.includes("join")){
+		walls["4,13,1"].node.remove();
+		unlocked.push(6);
+		document.getElementById('key').play();
+	}
+}
+function checkshirley2answer(){
+	var ans = document.getElementById('shirley2ans').value.toLowerCase();
+	if(ans.includes("beetroot")){
+		walls["2,14,0"].node.remove();
+		unlocked.push(61);
+		document.getElementById('key').play();
+	}
+}
+function checkshirley3answer(){
+	var ans = document.getElementById('shirley3ans').value.toLowerCase();
+	if(ans.includes("theobroma")){
+		walls["1,15,1"].node.remove();
+		unlocked.push(69);
+		document.getElementById('key').play();
+	}
+}
+function checkpurna1answer(){
+	var ans = document.getElementById('purna1ans').value.toLowerCase();
+	if(ans.includes("sexy") && ans.includes("legs")){
+		walls["4,13,3"].node.remove();
+		unlocked.push(5);
+		document.getElementById('key').play();
+	}
+}
+function checkpurna2answer(){
+	var ans = document.getElementById('purna2ans').value.toLowerCase();
+	if(ans.includes("khane")){
+		walls["1,12,3"].node.remove();
+		unlocked.push(51);
+		unlocked.push(59);
+		document.getElementById('key').play();
+	}
+}
+
 
 function startthings(i){
 	var stt = document.getElementById('starting');
@@ -272,8 +396,19 @@ window.onload = function() {
 	};
 	var running=false;
 	var pointer = { x: 0, y: 0 };
+	var bgm = document.getElementById('bgm');
+	bgm.volume = .3;
 
 	function regionUpdate(newregion){
+		if(!gameended){
+			if(unlocked.length>=16){
+				setTimeout(function(){
+					document.getElementById('gameendnotice').className += " inandout";
+					setTimeout(function() {document.getElementById('gameendnotice').className = "notice";}, 4000);
+				}, 2000);
+				gameended = true;
+			}
+		}
 		if(currentregion==newregion)	return;
 		else{
 			console.log("Entered ",newregion," from ",currentregion);
@@ -294,6 +429,22 @@ window.onload = function() {
 					sv.requestFullscreen().then(function() {sv.play();}, function() {
 						showplay();
 						currvid = 's2vid';
+					});
+					break;
+				case 59:
+					bgm.pause();
+					var pv = document.getElementById('pvid');
+					pv.requestFullscreen().then(function() {pv.play();}, function() {
+						showplay();
+						currvid = 'pvid';
+					});
+					break;
+				case 69:
+					bgm.pause();
+					var sdv = document.getElementById('sdvid');
+					sdv.requestFullscreen().then(function() {sdv.play();}, function() {
+						showplay();
+						currvid = 'sdvid';
 					});
 					break;
 				case 99:
@@ -322,6 +473,9 @@ window.onload = function() {
 						startbgm()
 						break;
 					case 49:
+						startbgm()
+						break;
+					case 69:
 						startbgm()
 						break;
 					case 99:
@@ -464,7 +618,7 @@ window.onload = function() {
 	bk2q.className = "question";
 	bk2q.innerHTML = `
 	Why do we always mess up birthdays of friends!?<br>
-	<input type='text' id='bk2ans' oninput='checkbk2answer()'>
+	<button>I don't mess up birthdays!</button><button onclick='checkbk2answer()'>&#x1F648 &#x1F648 &#x1F648</button>
 	`;
 	walls["18,2,1"].node.appendChild(bk2q);
 	addwall(20,3,3,'url("css3dlab_files/noentry.jpg")');
@@ -483,25 +637,93 @@ window.onload = function() {
 	bkvid.autoplay = 'false';
 	walls["20,2,3"].node.appendChild(bkvid);
 
-	// Gate for Suhail2 portion
+	// Gates for Suhail portion
 	addwall(14,24,2,'url("css3dlab_files/noentry.jpg")');
+	var suhail1q = document.createElement("div");
+	suhail1q.className = "question";
+	suhail1q.innerHTML = `
+	Fill in the blanks:<br>The quote near the beginning is from T_im_lewee_ Park<br>
+	<input type='text' id='suhail1ans' oninput='checksuhail1answer()'>
+	`;
+	walls["14,24,2"].node.appendChild(suhail1q);
+	addwall(17,24,2,'url("css3dlab_files/noentry.jpg")');
 	var suhail2q = document.createElement("div");
 	suhail2q.className = "question";
 	suhail2q.innerHTML = `
-	Fill in the blanks:<br>The quote near the beginning is from T_im_lewee_ Park<br>
+	One month ago, I was introduced to a song by which artist?<br>
 	<input type='text' id='suhail2ans' oninput='checksuhail2answer()'>
 	`;
-	walls["14,24,2"].node.appendChild(suhail2q);
-	walls["18,24,2"].node.innerHTML = "<span style='color: yellow'>I've never made anything this cool before!</span>";
-	walls["18,23,3"].node.innerHTML = "<span style='color: yellow'>**HUGS**</span>";
+	walls["17,24,2"].node.appendChild(suhail2q);
+	walls["18,24,2"].node.innerHTML = "<span style='color: yellow'>I've never made anything even remotely this cool before!</span>";
+	walls["20,23,2"].node.innerHTML = "<span style='color: yellow; font-size: 100px;'>&#x1F917</span>";
+	walls["18,23,0"].node.innerHTML = "<span style='color: yellow'>The month and day of the month right now match those of your birthday! :o</span>";
 	// Video for Suhail
 	var suhail2vid = document.createElement('video');
 	suhail2vid.id = 's2vid';
-	suhail2vid.src = 'css3dlab_files/svid.webm';
+	suhail2vid.src = 'css3dlab_files/svid.mp4';
 	suhail2vid.preload = 'auto';
 	suhail2vid.autoplay = 'false';
 	walls["20,25,1"].node.appendChild(suhail2vid);
 
+	// Gates for Shirley
+	addwall(4,13,1,'url("css3dlab_files/noentry.jpg")');
+	var shirley1q = document.createElement("div");
+	shirley1q.className = "question";
+	shirley1q.innerHTML = `
+	201217<br>
+	<input type='text' id='shirley1ans' oninput='checkshirley1answer()'>
+	`;
+	walls["4,13,1"].node.appendChild(shirley1q);
+	addwall(2,14,0,'url("css3dlab_files/noentry.jpg")');
+	var shirley2q = document.createElement("div");
+	shirley2q.className = "question";
+	shirley2q.innerHTML = `
+	What did we eat that affected how much oxygen our tissues get?<br>
+	<input type='text' id='shirley2ans' oninput='checkshirley2answer()'>
+	`;
+	walls["2,14,0"].node.appendChild(shirley2q);
+	addwall(1,15,1,'url("css3dlab_files/noentry.jpg")');
+	var shirley3q = document.createElement("div");
+	shirley3q.className = "question";
+	shirley3q.innerHTML = `
+	The place where we went on dates, when we were hysterical and when we just wanted to spend time together.<br>
+	<input type='text' id='shirley3ans' oninput='checkshirley3answer()'>
+	`;
+	walls["1,15,1"].node.appendChild(shirley3q);
+	// Video for Shirley
+	var shirleyvid = document.createElement('video');
+	shirleyvid.id = 'sdvid';
+	shirleyvid.src = 'css3dlab_files/sdvid.mp4';
+	shirleyvid.preload = 'auto';
+	shirleyvid.autoplay = 'false';
+	walls["1,17,1"].node.appendChild(shirleyvid);
+
+	// Gates for purna
+	addwall(4,13,3,'url("css3dlab_files/noentry.jpg")');
+	var purna1q = document.createElement("div");
+	purna1q.className = "question";
+	purna1q.innerHTML = `
+	What is your batchmate's nickname for you?<br>
+	<input type='text' id='purna1ans' oninput='checkpurna1answer()'>
+	`;
+	walls["4,13,3"].node.appendChild(purna1q);
+	addwall(1,12,3,'url("css3dlab_files/noentry.jpg")');
+	var purna2q = document.createElement("div");
+	purna2q.className = "question";
+	purna2q.innerHTML = `
+	From your batchmates, what do we do when we celebrate?<br>
+	Chalo _____ chalte hain.<br>
+	<input type='text' id='purna2ans' oninput='checkpurna2answer()'>
+	`;
+	walls["1,12,3"].node.appendChild(purna2q);
+	// Video for purna
+	var purnavid = document.createElement('video');
+	purnavid.id = 'pvid';
+	purnavid.src = 'css3dlab_files/pvid.mp4';
+	purnavid.preload = 'auto';
+	purnavid.autoplay = 'false';
+	walls["1,9,3"].node.appendChild(purnavid);
+	
 	// testwalls(1,1,0,1);
 	// testwalls(1,1,1,2);
 	// testwalls(1,1,2,3);
@@ -571,7 +793,8 @@ window.onload = function() {
 					videlem.play();
 				}
 				break;
-			case 77:
+			case 90:
+				showmusic();
 				var reg = findRegion(-viewport.camera.position.x, -viewport.camera.position.y);
 				if(reg%10!=9){
 					var bgm = document.getElementById('bgm');
@@ -649,6 +872,7 @@ window.onload = function() {
 			viewport.camera.position.y -= yo * speed;
 		}
 		viewport.camera.update();
+		updateMusicByDistances(-viewport.camera.position.x,-viewport.camera.position.y);
 		var region = findRegion(-viewport.camera.position.x, -viewport.camera.position.y);
 		regionUpdate(region);
 		if(keyState.debug)	console.log(viewport.camera.position);
